380dec104cbe620586fd483cd09b549064dc8ab3
Jeferson Sabino dos Santos Junior <86162489+JefersonTaiyou@users.noreply.github.com>
2023-03-15 20:46:46 -0300

    add project

---
+++ Smaug/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(move * speed, body.velocity.y);
+
+        // MOVE and Flip left-right
+        if (move > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (move > 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+
+        // JUMP
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
+    }
+}
